e498e7a58fe52164a6eb40ce9d5e7c2b446f15f8
madhank93 <>
2021-03-05 13:50:03 +0530

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: Routes.welcomeScreen,
+      routes: {
+        Routes.welcomeScreen: (context) => WelcomeScreen(),
+        Routes.loginScreen: (context) => LoginScreen(),
+        Routes.registrationScreen: (context) => RegistrationScreen(),
+        Routes.chatScreen: (context) => ChatScreen(),
+      },
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static String chatScreen = "chat_screen";
+  static String loginScreen = "login_screen";
+  static String registrationScreen = "registration_screen";
+  static String welcomeScreen = "welcome_screen";
+}
